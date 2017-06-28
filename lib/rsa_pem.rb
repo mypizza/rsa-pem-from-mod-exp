6ff32b1ae93545e6c6c950f24c8cb87b1489a5f0
Daniel Chang <daniel.chang85@gmail.com>
2017-06-28 13:57:17 -0400

    Rename and refactor RsaPem.

---
+++ lib/rsa_pem.rb
@@ -0,0 +1,86 @@
+require 'base64'
+
+class RsaPem
+  class << self
+    def from(mod, exp)
+      encoded_pubkey = create_encoded_pubkey_from(mod, exp)
+      der_b64 = hex_to_base64_digest encoded_pubkey
+
+      pem_padding_for der_b64
+    end
+
+    private
+
+    def create_encoded_pubkey_from(mod, exp)
+      modulus = Base64.urlsafe_decode64(mod)
+      exponent = Base64.urlsafe_decode64(exp)
+
+      modulus_hex = prepad_signed(base64_to_hex_digest modulus)
+      exponent_hex = prepad_signed(base64_to_hex_digest exponent)
+
+      modlen = modulus_hex.length / 2
+      explen = exponent_hex.length / 2
+
+      encoded_modlen = encode_length_hex(modlen)
+      encoded_explen = encode_length_hex(explen)
+
+      full_hex_len = modlen + explen + encoded_modlen.length/2 + encoded_explen.length/2 + 2
+      encoded_hex_len = encode_length_hex(full_hex_len)
+
+      "30#{encoded_hex_len}02#{encoded_modlen}#{modulus_hex}02#{encoded_explen}#{exponent_hex}"
+    end
+
+    def pem_padding_for(der_b64)
+      "-----BEGIN RSA PUBLIC KEY-----\n#{add_newlines der_b64}\n-----END RSA PUBLIC KEY-----"
+    end
+
+    def add_newlines(der_b64)
+      der_b64.scan(/.{1,64}/).join("\n")
+    end
+
+    def prepad_signed(hex)
+      msb = hex[0]
+      if (msb < '0' || msb > '7')
+        pad(hex, '00')
+      else
+       hex
+      end
+    end
+
+    def encode_length_hex(der_len)
+      # encode ASN.1 DER length field
+
+      if short_form? der_len
+        int_to_hex(der_len)
+      else
+        number_hex = int_to_hex(der_len)
+        length_of_length_byte = 128 + number_hex.length/2
+
+        "#{int_to_hex(length_of_length_byte)}#{number_hex}"
+      end
+    end
+
+    def short_form?(asn1_der_length)
+      asn1_der_length <= 127
+    end
+
+    def pad(hex, padding)
+      "#{padding}#{hex}"
+    end
+
+    def int_to_hex(int)
+      number_hex = int.to_s(16)
+      number_hex.length.odd? ? pad(number_hex, '0') : number_hex
+    end
+
+    def hex_to_base64_digest(hex)
+      # https://stackoverflow.com/questions/9986971/converting-a-hexadecimal-digest-to-base64-in-ruby
+      [[hex].pack("H*")].pack("m0")
+    end
+
+    def base64_to_hex_digest(base64)
+      # http://anthonylewis.com/2011/02/09/to-hex-and-back-with-ruby/
+      base64.unpack('H*').first
+    end
+  end
+end
--- spec/pem_spec.rb
+++ spec/pem_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
-require_relative '../lib/pem'
+require_relative '../lib/rsa_pem'
 
-RSpec.describe RsaPemFromModExp::Pem do
+RSpec.describe RsaPem do
   let(:modulus) { 'iz7sYHwIcY9XIvDJ_s7ujJnL-OPrSApxyR9cUmhB_YazFhFQOEBHviib46CwY-RLfTfsL6tBwZsI10RtgwVUGjZZ41G6gaZwzGtrrwRvx22_X1GROb1UO5wu0GVJKSD_9Hf7-SnzwkzJ73OiQAFkegiKqghDCRKRxlKQzRFIpdgXuSfdDxy7MPhhKC3YGfKj2S4jaNzNxFJm-8dY-oa2-7f2qQvgXvVnEnFG72FbJChy_Ol7cXkpXf03v6gRMOm8dQIrWh3VhRSm5HJNoOZb2X8Ubc6o-6CDN7OJFZMKlgNuLKT3s3xpY5j7qbvhMANOhAgPpYlDJTR9FHFFFRPFew' }
   let(:exponent) { 'AQAB' }
   let(:valid_pem) { "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAiz7sYHwIcY9XIvDJ/s7ujJnL+OPrSApxyR9cUmhB/YazFhFQOEBH\nviib46CwY+RLfTfsL6tBwZsI10RtgwVUGjZZ41G6gaZwzGtrrwRvx22/X1GROb1U\nO5wu0GVJKSD/9Hf7+SnzwkzJ73OiQAFkegiKqghDCRKRxlKQzRFIpdgXuSfdDxy7\nMPhhKC3YGfKj2S4jaNzNxFJm+8dY+oa2+7f2qQvgXvVnEnFG72FbJChy/Ol7cXkp\nXf03v6gRMOm8dQIrWh3VhRSm5HJNoOZb2X8Ubc6o+6CDN7OJFZMKlgNuLKT3s3xp\nY5j7qbvhMANOhAgPpYlDJTR9FHFFFRPFewIDAQAB\n-----END RSA PUBLIC KEY-----" }
 
   it 'generates a valid PEM from a modulus and exponent' do
-    pem = RsaPemFromModExp::Pem.from modulus, exponent
+    pem = RsaPem.from modulus, exponent
     expect(pem).to eq valid_pem
   end
 end
